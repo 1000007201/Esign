7e8bc1b3d641b66ada06d331aa33eecd3446ac93
1000007201 <nishant.sharma5504@gmail.com.com>
2025-09-01 15:52:01 +0530

    Added clear pad and download sign as pdf functionality

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 
-let drawing = false;
-let canvas  = document.getElementById("pad");
-const ctx   = canvas.getContext("2d"); //Provide us drawing tools
+let drawing = false; // Initailly we are not drawing
+
+// Getting all DOM objects
+let canvas   = document.getElementById("pad");
+let ClearBtn = document.getElementById("clear-btn");
+let SaveBtn  = document.getElementById("save-btn");
+
+const ctx    = canvas.getContext("2d"); //Provide us drawing tools
+
+// Adding the event listeners
+SaveBtn.addEventListener("click", save_sign);
+ClearBtn.addEventListener("click", clear_pad);
 canvas.addEventListener("mousedown", ()=> drawing = true);
 canvas.addEventListener("mouseup", stop_draw);
 canvas.addEventListener("mousemove", draw);
@@ -27,3 +36,13 @@ function draw(e){
     // lineTo, stroke, beginPath, moveTo are responsible for smooth line
 }
 
+function clear_pad(e){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+function save_sign(e){
+    let link      = document.createElement("a");
+    link.download = "signture.png"; // It will be the filename
+    link.href     = canvas.toDataURL("image/png");
+    link.click();
+};
